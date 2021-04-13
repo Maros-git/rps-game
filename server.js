cee0d7e0c594c297e0de3d79bd95cf2500f8208b
Maros-git <77161558+Maros-git@users.noreply.github.com>
2021-04-14 01:01:23 +0200

    another try 3

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {res.send('WORKING!') }),
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is listening on port ${process.env.PORT}`);
