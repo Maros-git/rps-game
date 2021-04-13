86f1426bf51387b64c9a577e6ee52c734f1eed66
Maros-git <77161558+Maros-git@users.noreply.github.com>
2021-04-14 01:17:21 +0200

    another try 7

--- server.js
+++ server.js
@@ -1,12 +1,8 @@
 import express from 'express';
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; 
-
 const app = express();
 
 app.use(express.static('public'));
-
-app.get('/', (_req, res) => {res.send('WORKING!') }),
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is listening on port ${process.env.PORT}`);
